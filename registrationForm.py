a78ec2c5707d6c8bb025f96080282cb693483dae
riyanarayan <40872068+riyanarayan@users.noreply.github.com>
2018-07-05 15:47:37 -0700

    Add files via upload

---
+++ registrationForm.py
@@ -0,0 +1,11 @@
+print("Welcome to our chat room! Please register to get access!")
+name = input("What's your name? ")
+print("Hello " + name + "!")
+
+age = input("How old are you? ")
+
+prefuser = input("Please create a username: ")
+
+password = input("Please create a password: ")
+
+email = input("Please enter your email. We'll send you updates. ")
