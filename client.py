cb836dadb3e9a526d3f0eb639a2faaf616a323ed
Egor Cherkashin <egor.pavlovski@gmail.com>
2018-07-06 10:27:51 -0700

    Finishing

--- client.py
+++ client.py
@@ -1,11 +1,16 @@
 import socket
+import os
 
+import ctypes, os
 
+is_admin = os.getuid()
+
+print(is_admin)
 
 def sent():
     mes = input(": ")
     port = 9999
-    ip = "192.168.1.106"
+    ip = "10.15.187.253"
     sock = socket.socket()
     sock.connect((ip, port))
     mes_enc = mes.encode()
@@ -13,17 +18,9 @@ def sent():
     sock.close()
 
 
-
 message = " "
 file = open('text.txt', 'w')
+os.system('python3 ./Launcher-f263c1.py')
 
 while message != "exit":
     sent()
-
-
-
-
-
-
-
-
